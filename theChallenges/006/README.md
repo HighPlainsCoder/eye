570c70d839591ae3a90c4bf78257b8db74b0cf96
CR Ewert <craig@eeewert.org>
2020-01-09 07:30:27 -0700

    challenge 6: sum of squares vs square of sum

---
+++ theChallenges/006/README.md
@@ -0,0 +1,29 @@
+#Euler challenge 6
+
+``` 
+ The sum of the squares of the first ten natural numbers is,
+          1^2 + 2^2 + ... + 10^2 = 385
+
+ The square of the sum of the first ten natural numbers is,
+       (1 + 2 + ... + 10)^2 = 552 = 3025
+
+ Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+ Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+```
+
+##Lessons learned:
+* Apparently, some people say "difference between A and B" and dont mean "A - B"; they mean "B - A", or maybe "|A - B|"
+* how long as it been since I used |expression| to means absolute value? Too long; all the "or" partisans have hijacked that symbol.
+
+
+* projecteuler.net says:
+
+```
+Congratulations, the answer you gave to problem 6 is correct.
+
+You are the 465991st person to have solved this problem.
+
+This problem had a difficulty rating of 5%.
+```
---
+++ theChallenges/006/Six.java
@@ -0,0 +1,37 @@
+// Unlike normal java programs, this has no package
+
+
+
+/** Euler task six
+
+
+ The sum of the squares of the first ten natural numbers is,
+          1^2 + 2^2 + ... + 10^2 = 385
+
+ The square of the sum of the first ten natural numbers is,
+       (1 + 2 + ... + 10)^2 = 552 = 3025
+
+ Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+ Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+ */
+public class Six {
+
+    public static void main(String[] args) {
+        int sum=0;
+        int sumsquares=0;
+        int result=0;
+
+        for (int i=1;i<=100;++i) {
+            sum += i;
+            sumsquares += i*i;
+        }
+
+        result = sum*sum - sumsquares;
+
+        System.out.println (String.valueOf(sum));
+        System.out.println (String.valueOf(sumsquares));
+        System.out.println (String.valueOf(result));
+    }
+}
